84fbb5969b010426ee2c4c649a6eb92364424719
Matt Barnfield <matt.barnfield@softwire.com>
2020-10-28 11:11:13 +0000

    Add intentional error to test failure slack message

--- DotnetTemplate.Web.Tests/Controllers/HomeControllerTests.cs
+++ DotnetTemplate.Web.Tests/Controllers/HomeControllerTests.cs
@@ -37,7 +37,7 @@ namespace DotnetTemplate.Web.Tests.Controllers
             // Then
             var expectedModel = new SecondPageViewModel();
             result.Should().BeViewResult()
-                .Model.Should().BeEquivalentTo(expectedModel);
+                .Model.Should().BeEquivalentTo(null);
         }
     }
 }
